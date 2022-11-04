3a40645c45c8bd80c0fa957d900cf1cba928ab45
Mario Carrion <mario@carrion.ws>
2022-11-04 00:35:10 -0400

    Unit Testing Unary gRPC Service in Go
    https://youtu.be/_1ZmwV1Fo4Y

---
+++ examples/server/main_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+
+	userpb "github.com/MarioCarrion/grpc-microservice-example/gen/go/user/v1"
+)
+
+func TestUserService_GetUser(t *testing.T) {
+	//- Server Initialization
+	lis := bufconn.Listen(1024 * 1024)
+	t.Cleanup(func() {
+		lis.Close()
+	})
+
+	srv := grpc.NewServer()
+	t.Cleanup(func() {
+		srv.Stop()
+	})
+
+	svc := userService{}
+	userpb.RegisterUserServiceServer(srv, &svc)
+
+	go func() {
+		if err := srv.Serve(lis); err != nil {
+			log.Fatalf("srv.Serve %v", err)
+		}
+	}()
+
+	//- Test
+	dialer := func(context.Context, string) (net.Conn, error) {
+		return lis.Dial()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	t.Cleanup(func() {
+		cancel()
+	})
+
+	conn, err := grpc.DialContext(ctx, "", grpc.WithContextDialer(dialer), grpc.WithInsecure())
+	t.Cleanup(func() {
+		conn.Close()
+	})
+	if err != nil {
+		t.Fatalf("grpc.DialContext %v", err)
+	}
+
+	client := userpb.NewUserServiceClient(conn)
+	res, err := client.GetUser(context.Background(), &userpb.GetUserRequest{Uuid: "123"})
+	if err != nil {
+		t.Fatalf("client.GetUser %v", err)
+	}
+
+	if res.User.Uuid != "123" && res.User.FullName != "Mario" {
+		t.Fatalf("Unexpected values %v", res.User)
+	}
+}
