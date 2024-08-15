76d02a929aa2ce71fda11896999a236af685001e
Mario Carrion <mario@carrion.ws>
2024-08-14 23:49:15 -0400

    chore(cleanup): GA Go caching, yamllinter, Makefile
    * Github Actions configured to cache.
    * New yamllinter to lint all YAML files.
    * Makefile updated to use PHONY.

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -3,6 +3,7 @@ package tools
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"                     // Buf CLI
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint" // Linter
+	_ "github.com/google/yamlfmt/cmd/yamlfmt"               // YAML Formatter
 	_ "github.com/grpc-ecosystem/grpc-health-probe"         // gRPC Health Check
 	_ "golang.org/x/vuln/cmd/govulncheck"                   // Official Go vulnerability checks
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"       // Protocol Buffer Plugin
